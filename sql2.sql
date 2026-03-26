a240ccc1218cbd5a4e2b59fa761ff6a1edbf3994
dobeduyen111-dot <dobeduyen111@gmail.com>
2026-03-26 21:54:59 +0700

    Add files via upload

---
+++ sql2.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict po3V6mVRfK1XhobxWgx5nGB3SbmelWUzHLKbEuMZGaQTBViOpOUdRUWCTW9xH2i
+
+-- Dumped from database version 18.1
+-- Dumped by pg_dump version 18.1
+
+-- Started on 2026-02-05 23:17:31
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-02-05 23:17:31
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict po3V6mVRfK1XhobxWgx5nGB3SbmelWUzHLKbEuMZGaQTBViOpOUdRUWCTW9xH2i
+
